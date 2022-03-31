dbdb2d29afa1a0a275999184bfcbd83ff4b309af
Biinod81 <thomas.ly@iologo.io>
2022-03-31 14:58:37 +0200

    add: location on searchFilters

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
 import Image from 'next/image';
+import { useContext } from 'react';
 import { Box, Icon, Flex } from '@chakra-ui/react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
